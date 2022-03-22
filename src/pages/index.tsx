a5464baa85105c670b0cc0753ab0b3f82ccfa7c8
earsenii <ermolarsen@mail.ru>
2022-03-22 22:52:07 +0300

    locales updated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import {useRouter} from 'next/router';
 import {serverSideTranslations} from 'next-i18next/serverSideTranslations';
 import {useTranslation} from 'next-i18next';
 import {Text} from '@mantine/core';
@@ -6,7 +5,6 @@ import {Text} from '@mantine/core';
 import {ColorSchemeToggle} from '../components/ColorSchemeToggle';
 
 export default function HomePage() {
-    const router = useRouter();
     const {t} = useTranslation('common');
     return (
         <>
@@ -17,7 +15,25 @@ export default function HomePage() {
                 sx={{maxWidth: 580, fontSize: 34, fontWeight: 600}}
                 mx="auto"
                 mt="xl">
-                {t('utils.login')}
+                {t('login')}
+            </Text>
+            <Text
+                color="dimmed"
+                align="center"
+                size="lg"
+                sx={{maxWidth: 580, fontSize: 34, fontWeight: 600}}
+                mx="auto"
+                mt="xl">
+                {t('date_of_registration')}
+            </Text>
+            <Text
+                color="dimmed"
+                align="center"
+                size="lg"
+                sx={{maxWidth: 580, fontSize: 34, fontWeight: 600}}
+                mx="auto"
+                mt="xl">
+                {t('contribute')}
             </Text>
             <ColorSchemeToggle />
         </>
