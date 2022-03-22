623ff843c4c5e8ff29d1619599e1eecac799027b
earsenii <ermolarsen@mail.ru>
2022-03-22 18:21:02 +0300

    implemented i18n in the first way

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,15 @@
-import {Text} from '@mantine/core';
+import Link from 'next/link';
+import {useRouter} from 'next/router';
 
+import {useTranslation} from 'next-i18next';
+import {serverSideTranslations} from 'next-i18next/serverSideTranslations';
+
+import {Text, Button} from '@mantine/core';
 import {ColorSchemeToggle} from '../components/ColorSchemeToggle';
 
 export default function HomePage() {
+    const router = useRouter();
+    const {t} = useTranslation('common');
     return (
         <>
             <Text
@@ -12,9 +19,22 @@ export default function HomePage() {
                 sx={{maxWidth: 580, fontSize: 34, fontWeight: 600}}
                 mx="auto"
                 mt="xl">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                {t('utils.login')}
             </Text>
+            <Link href="/" locale={router.locale === 'en' ? 'ru' : 'en'}>
+                <Button color="gray" radius="md" size="md">
+                    {t('change')}
+                </Button>
+            </Link>
             <ColorSchemeToggle />
         </>
     );
+}
+
+export async function getStaticProps({locale}) {
+    return {
+        props: {
+            ...(await serverSideTranslations(locale, ['common'])),
+        },
+    };
 }
