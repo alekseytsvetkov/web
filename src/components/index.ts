fa0ac82dbb979137c811d928d1fd1b806d06f06b
earsenii <ermolarsen@mail.ru>
2022-03-24 18:20:15 +0300

    created header for homepage

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,1 @@
 export * from './ColorSchemeToggle';
-export * from './Header';
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,46 +1,70 @@
 import {FC} from 'react';
 
 import {GetStaticProps} from 'next/types';
+import Link from 'next/link';
 import {serverSideTranslations} from 'next-i18next/serverSideTranslations';
 import {useTranslation} from 'next-i18next';
-import {Text} from '@mantine/core';
-import {MainLayout} from '../layouts';
+import {AppShell, Button, Header, Text} from '@mantine/core';
 
-import {ColorSchemeToggle} from '../components/ColorSchemeToggle';
+import {ColorSchemeToggle} from '../components';
 
 const HomePage: FC = () => {
     const {t} = useTranslation('common');
+
     return (
-        <MainLayout>
-            <Text
-                color="dimmed"
-                align="center"
-                size="lg"
-                sx={{maxWidth: 580, fontSize: 34, fontWeight: 600}}
-                mx="auto"
-                mt="xl">
-                {t('login')}
-            </Text>
-            <Text
-                color="dimmed"
-                align="center"
-                size="lg"
-                sx={{maxWidth: 580, fontSize: 34, fontWeight: 600}}
-                mx="auto"
-                mt="xl">
-                {t('date_of_registration')}
-            </Text>
-            <Text
-                color="dimmed"
-                align="center"
-                size="lg"
-                sx={{maxWidth: 580, fontSize: 34, fontWeight: 600}}
-                mx="auto"
-                mt="xl">
-                {t('contribute')}
-            </Text>
-            <ColorSchemeToggle />
-        </MainLayout>
+        <AppShell
+            fixed
+            header={
+                <Header height={70} p="md">
+                    <div
+                        style={{
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'space-between',
+                            height: '100%',
+                        }}>
+                        <Link href="/">
+                            <div
+                                style={{
+                                    lineHeight: 1,
+                                    cursor: 'pointer',
+                                }}>
+                                <Text sx={() => ({lineHeight: 1})} size="xl" weight={700}>
+                                    Skeetry
+                                </Text>
+                                <Text color="dimmed" size="xs">
+                                    {t('description')}
+                                </Text>
+                            </div>
+                        </Link>
+
+                        <div
+                            style={{
+                                display: 'flex',
+                                alignItems: 'center',
+                                justifyContent: 'space-between',
+                                width: 130,
+                            }}>
+                            <ColorSchemeToggle />
+                            <Button
+                                sx={(theme) => ({
+                                    color: theme.colorScheme === 'dark' ? 'black' : 'dark',
+                                    backgroundColor: theme.colorScheme === 'dark' ? 'white' : 'dark',
+                                    '&:hover': {
+                                        backgroundColor:
+                                            theme.colorScheme === 'dark' ? theme.colors.gray[0] : theme.colors.dark[9],
+                                    },
+                                })}
+                                color="dark"
+                                radius={12}>
+                                {t('login')}
+                            </Button>
+                        </div>
+                    </div>
+                </Header>
+            }>
+            <Text>{t('welcome')}</Text>
+        </AppShell>
     );
 };
 
