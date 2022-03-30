ab53e912e707e29325f77b9d056836a62490b19b
earsenii <ermolarsen@mail.ru>
2022-03-30 10:25:35 +0300

    create user mock

---
+++ __mocks__/user.ts
@@ -0,0 +1,20 @@
+export const MOCK_USER = [
+    {
+        id: '1',
+        name: 'Aleksey Tsvetkov',
+        username: 'kive',
+        location: 'Saint-Petersburg, Russia',
+        createdAt: new Date(),
+        followersCount: 74,
+        followingCount: 28,
+    },
+    {
+        id: '2',
+        name: 'Arsenii Ermolaev',
+        username: 'earsenii',
+        location: 'Yaroslavl, Russia',
+        createdAt: new Date(),
+        followersCount: 24,
+        followingCount: 33,
+    },
+];
