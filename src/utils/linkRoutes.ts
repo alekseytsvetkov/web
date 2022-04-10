59bd34b5a0a203f142128abdf7df86760be7d613
earsenii <ermolarsen@mail.ru>
2022-04-10 18:41:20 +0300

    fix link routes

--- src/utils/linkRoutes.ts
+++ src/utils/linkRoutes.ts
@@ -2,11 +2,11 @@ export const linkRoutes = [
     {
         id: 1,
         name: 'want',
-        url: '/@username/want',
+        url: '/want',
     },
     {
         id: 2,
         name: 'visited',
-        url: '/@username/visited',
+        url: '/visited',
     },
 ];
